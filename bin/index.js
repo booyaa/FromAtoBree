c8c55a6baa09ffc93e5f8a776426e5a2bed1836f
Mark Sta Ana <booyaabooyaabooyaa@gmail.com>
2014-06-27 19:12:56 +0100

    initial commit of binary, removed more debugging from lib

--- bin/index.js
+++ bin/index.js
@@ -1,88 +1,91 @@
 #!/usr/bin/env node
 
-var dijkstra = require('dijkstrajs') // we have a winner
-  , find_path = dijkstra.find_path;
-var options = { level : 69, swiftTravel : true };
+var fatb = require('../lib/fromatobree'),
+    path = require('path');
 
-function CreateGraph() {
-  //--------------- stable parser -----------------
-  // How to reproduce stable_raw.js
-  // * stripped just Locs out of TR_Data.lua
-  // * pipe Stables.lua to stable_raw.js
-
-  var stables = require('./stable_raw.js');
-
-  var graph = {};
-
-  for(var loc in stables) {
-
-    graph[loc] = {};
-    var destinations = {};
-
-    for (var dest in stables[loc].d) {
-
-      //i swift travel (parameter)
-      var stCost = typeof(stables[loc].d[dest].st) !== "undefined" ? 1 : 5;
-
-      // level costing (parameter)
-      var level = options.level;
-      
-      // TODO: standing or reqs costs
-
-      var minLevel = 0;
-      if (typeof(stables[loc].d[dest].l) !== "undefined") {
-        minLevel = stables[loc].d[dest].l;
-        // console.log("dest %s\n\tl %d + ml %d = %d", dest, level, minLevel, (level + minLevel));        
-        // console.log("\tl < ml %s st %d string %s", (level < minLevel), stCost, minLevel.toString());
-      }
-
-      var levelCost = level < minLevel ? 5 : 0; // zero otherwise we'll penalise the route unecessarily because of the previous swift travel weighting
-
-      var weighting = stCost + levelCost;
-
-      destinations[dest] = weighting;
-    }
-    graph[loc] = destinations;
-  }
-
-  return graph;
-}
-
-function GetPathCostObject(path) {
-  var start = path[0];
-  var next = start;
-  var costObject = {"start": start};
-
-  console.log("Start: %j", start);
-  var keyname = "via ";
-  for(var i=1; i<path.length; i++) {
-    var dest = path[i];
-
-    var via = stables[next].d[dest];
-    costObject[keyname + dest] = via;
- 
-    next = dest;
-  }
-
-  return costObject;
-}
-
-
-function GetPlace(place) {
-  var regex = new RegExp("^"+place,"i");
-
-  var found = [];
-  Object.keys(graph).forEach(function(name) {
-    if (regex.test(name)) {
-      found.push(name);
-    }
-  });
-
-  return found;
-}
-
-var graph = CreateGraph();
-var path = require('path');
+// var dijkstra = require('dijkstrajs') // we have a winner
+//   , find_path = dijkstra.find_path;
+// var options = { level : 69, swiftTravel : true };
+// 
+// function CreateGraph() {
+//   //--------------- stable parser -----------------
+//   // How to reproduce stable_raw.js
+//   // * stripped just Locs out of TR_Data.lua
+//   // * pipe Stables.lua to stable_raw.js
+// 
+//   var stables = require('./stable_raw.js');
+// 
+//   var graph = {};
+// 
+//   for(var loc in stables) {
+// 
+//     graph[loc] = {};
+//     var destinations = {};
+// 
+//     for (var dest in stables[loc].d) {
+// 
+//       //i swift travel (parameter)
+//       var stCost = typeof(stables[loc].d[dest].st) !== "undefined" ? 1 : 5;
+// 
+//       // level costing (parameter)
+//       var level = options.level;
+//       
+//       // TODO: standing or reqs costs
+// 
+//       var minLevel = 0;
+//       if (typeof(stables[loc].d[dest].l) !== "undefined") {
+//         minLevel = stables[loc].d[dest].l;
+//         // console.log("dest %s\n\tl %d + ml %d = %d", dest, level, minLevel, (level + minLevel));        
+//         // console.log("\tl < ml %s st %d string %s", (level < minLevel), stCost, minLevel.toString());
+//       }
+// 
+//       var levelCost = level < minLevel ? 5 : 0; // zero otherwise we'll penalise the route unecessarily because of the previous swift travel weighting
+// 
+//       var weighting = stCost + levelCost;
+// 
+//       destinations[dest] = weighting;
+//     }
+//     graph[loc] = destinations;
+//   }
+// 
+//   return graph;
+// }
+// 
+// function GetPathCostObject(path) {
+//   var start = path[0];
+//   var next = start;
+//   var costObject = {"start": start};
+// 
+//   console.log("Start: %j", start);
+//   var keyname = "via ";
+//   for(var i=1; i<path.length; i++) {
+//     var dest = path[i];
+// 
+//     var via = stables[next].d[dest];
+//     costObject[keyname + dest] = via;
+//  
+//     next = dest;
+//   }
+// 
+//   return costObject;
+// }
+// 
+// 
+// function GetPlace(place) {
+//   var regex = new RegExp("^"+place,"i");
+// 
+//   var found = [];
+//   Object.keys(graph).forEach(function(name) {
+//     if (regex.test(name)) {
+//       found.push(name);
+//     }
+//   });
+// 
+//   return found;
+// }
+// 
+// var graph = CreateGraph();
+// var path = require('path');
 
 if (process.argv.length < 3) {
   console.log("usage:");
@@ -96,43 +99,27 @@ var finishArg = process.argv[3];
 
 console.log("args start %s finish %s", startArg, finishArg);
 
-var start = GetPlace(startArg)[0]; // get first
-var finish = GetPlace(finishArg)[0]; // get first
+var start = fatb.GetPlace(startArg)[0]; // get first
+var finish = fatb.GetPlace(finishArg)[0]; // get first
 
 if (typeof(finishArg) === "undefined") {
-  console.log("Matches for %s %s", startArg, JSON.stringify(GetPlace(startArg)))
+  console.log("Matches for %s %s", startArg, JSON.stringify(fatb.GetPlace(startArg)))
   return;
 }
-console.log("GetPlace start %s finish %s", start, finish);
+console.log("GetPlace start: %s / finish: %s", start, finish);
 
-if (typeof start !== "undefined" && typeof finish !== "undefined") {
-  var path = find_path(graph, start, finish);
-  console.log("your route: ", path);
 
-  console.log('--------');
-  var costObject = GetPathCostObject(path); 
-  console.dir(costObject);
-
-  var cost = 0;
-  var time = 0;
-
-  for (var place in costObject ) {
-    var hasCost = costObject[place].hasOwnProperty("c");
-    var hasSTCost = costObject[place].hasOwnProperty("s");
-    var hasTime = costObject[place].hasOwnProperty("t");
-    var hasSTTime = costObject[place].hasOwnProperty("st");
-
-    console.log("place %s cost %d obj %j", place, hasCost, costObject[place]);
-
-    cost += hasCost ? parseInt(costObject[place].c) : 0;
-    cost += hasSTCost ? parseInt(costObject[place].s) : 0;
-
-    time += hasTime ? parseInt(costObject[place].t) : 0;
-    time += hasSTTime ? parseInt(costObject[place].st) : 0;
-  }
-
-  console.log("cost %d time %s", cost, time);
-
-} else {
+if (typeof start === "undefined" || typeof finish === "undefined") {
   console.log("No matches for either start (%s) or finish (%s) locations", startArg, finishArg);
+  return;
 }
+
+var route  = fatb.FindPath(start, finish);
+console.log("your route: ", route);
+
+var routeCost = fatb.GetPathCostObject(route); 
+
+var cost = fatb.GetTotalCost(routeCost);
+var time = fatb.GetTotalTime(routeCost);
+
+console.log("Total cost for route %d, the journey will take time %d seconds or  %d mins", cost, time, Math.round(time/60));
