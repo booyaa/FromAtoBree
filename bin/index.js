58891de9fa867e079e135b64a348492c15f97ddb
Mark Sta Ana <booyaabooyaabooyaa@gmail.com>
2014-06-30 08:41:30 +0100

    remove console logs and old commented code

--- bin/index.js
+++ bin/index.js
@@ -3,90 +3,6 @@
 var fatb = require('../lib/fromatobree'),
     path = require('path');
 
-// var dijkstra = require('dijkstrajs') // we have a winner
-//   , find_path = dijkstra.find_path;
-// var options = { level : 69, swiftTravel : true };
-// 
-// function CreateGraph() {
-//   //--------------- stable parser -----------------
-//   // How to reproduce stable_raw.js
-//   // * stripped just Locs out of TR_Data.lua
-//   // * pipe Stables.lua to stable_raw.js
-// 
-//   var stables = require('./stable_raw.js');
-// 
-//   var graph = {};
-// 
-//   for(var loc in stables) {
-// 
-//     graph[loc] = {};
-//     var destinations = {};
-// 
-//     for (var dest in stables[loc].d) {
-// 
-//       //i swift travel (parameter)
-//       var stCost = typeof(stables[loc].d[dest].st) !== "undefined" ? 1 : 5;
-// 
-//       // level costing (parameter)
-//       var level = options.level;
-//       
-//       // TODO: standing or reqs costs
-// 
-//       var minLevel = 0;
-//       if (typeof(stables[loc].d[dest].l) !== "undefined") {
-//         minLevel = stables[loc].d[dest].l;
-//         // console.log("dest %s\n\tl %d + ml %d = %d", dest, level, minLevel, (level + minLevel));        
-//         // console.log("\tl < ml %s st %d string %s", (level < minLevel), stCost, minLevel.toString());
-//       }
-// 
-//       var levelCost = level < minLevel ? 5 : 0; // zero otherwise we'll penalise the route unecessarily because of the previous swift travel weighting
-// 
-//       var weighting = stCost + levelCost;
-// 
-//       destinations[dest] = weighting;
-//     }
-//     graph[loc] = destinations;
-//   }
-// 
-//   return graph;
-// }
-// 
-// function GetPathCostObject(path) {
-//   var start = path[0];
-//   var next = start;
-//   var costObject = {"start": start};
-// 
-//   console.log("Start: %j", start);
-//   var keyname = "via ";
-//   for(var i=1; i<path.length; i++) {
-//     var dest = path[i];
-// 
-//     var via = stables[next].d[dest];
-//     costObject[keyname + dest] = via;
-//  
-//     next = dest;
-//   }
-// 
-//   return costObject;
-// }
-// 
-// 
-// function GetPlace(place) {
-//   var regex = new RegExp("^"+place,"i");
-// 
-//   var found = [];
-//   Object.keys(graph).forEach(function(name) {
-//     if (regex.test(name)) {
-//       found.push(name);
-//     }
-//   });
-// 
-//   return found;
-// }
-// 
-// var graph = CreateGraph();
-// var path = require('path');
-
 if (process.argv.length < 3) {
   console.log("usage:");
   console.log("%s <place> - lookup place", path.basename(process.argv[1]));
@@ -97,8 +13,6 @@ if (process.argv.length < 3) {
 var startArg = process.argv[2];
 var finishArg = process.argv[3];
 
-console.log("args start %s finish %s", startArg, finishArg);
-
 var start = fatb.GetPlace(startArg)[0]; // get first
 var finish = fatb.GetPlace(finishArg)[0]; // get first
 
@@ -107,7 +21,6 @@ if (typeof(finishArg) === "undefined") {
   return;
 }
 console.log("GetPlace start: %s / finish: %s", start, finish);
-
 
 if (typeof start === "undefined" || typeof finish === "undefined") {
   console.log("No matches for either start (%s) or finish (%s) locations", startArg, finishArg);
