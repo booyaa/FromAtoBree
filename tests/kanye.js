ccb6884c7989db79ba9456418d0c869b2deb2895
Mark Sta Ana <booyaabooyaabooyaa@gmail.com>
2014-07-09 08:47:15 +0100

    code cleanup, start to test for errors

--- tests/kanye.js
+++ tests/kanye.js
@@ -5,18 +5,38 @@ var test = require('tape').test;
 // for path finding tests
 var fatb = require('../lib/fromatobree');
 
-test("create graph with options, but level reqs not met", function(t) {
-  var options = fatb.setup({level: 29});
-  var graph = fatb.createGraph(true);
+var start = "Ost Galadh";
+var finish = "Mekhem-bizru";
 
-  t.equal(graph["West Bree"]["Esteldin"], 101);
+// test("find a route,but with level reqs, but not quest reqs met", function(t) {
+//   var options = fatb.setup({level: 50});
+//   fatb.graph = fatb.createGraph(true);
+// 
+//   t.deepEquals(fatb.FindPath(start,finish), ["Ost Galadh", "Mekhem-bizru"]);
+//   t.end();
+// });
+
+test("fail to find a route with level reqs, but not quest reqs met", function(t) {
+  var options = fatb.setup({level: 50});
+  fatb.graph = fatb.createGraph(true);
+  t.throws(fatb.FindPath(start,finish), "Error: Could not find a path from Ost Galadh to Mekhem-bizru");
   t.end();
 });
 
-test("create graph with options, but standing reqs met", function(t) {
-  var options = fatb.setup({standing: ["R28","Q6"]});
-  var graph = fatb.createGraph(true);
 
-  t.equal(graph["Harwick"]["Hytbold"], 1);
+test("find a route,but with both level and quest reqs met", function(t) {
+  var options = fatb.setup({level: 50, standing: ["Q3"]});
+  fatb.graph = fatb.createGraph(true);
+
+  t.deepEquals(fatb.FindPath(start,finish), ["Ost Galadh", "Echad Sirion", "The Vinyards of Lorien", "Mekhem-bizru"]);
   t.end();
 });
+
+
+// test("create graph with options, but standing reqs met", function(t) {
+//   var options = fatb.setup({standing: ["R28","Q6"]});
+//   var graph = fatb.createGraph(true);
+// 
+//   t.equal(graph["Harwick"]["Hytbold"], 1);
+//   t.end();
+// });
