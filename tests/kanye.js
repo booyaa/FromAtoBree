a873abebf596a73530e8da85286f8591af87e584
Mark Sta Ana <booyaabooyaabooyaa@gmail.com>
2014-07-09 08:10:39 +0100

    split setup tests to own file

--- tests/kanye.js
+++ tests/kanye.js
@@ -1,51 +1,15 @@
 /*jslint node: true, laxcomma: true, loopfunction: true, sub: true*/
 var test = require('tape').test;
 
+
+// for path finding tests
 var fatb = require('../lib/fromatobree');
-
-// create graph with and w/o options
-
-test("we have options", function(t) {
-  var options = fatb.setup({level: 50, standing: ["R1","R2"]});
-  t.deepEquals(options, {level: 50, standing: ["R1","R2"]});
-  t.end();
-});
-
-test("we do not have options", function(t) {
-  var options =  fatb.setup();
-  t.deepEquals(options, {});
-  t.end();
-});
-
-test("create graph with no options", function(t) {
-  var graph = fatb.createGraph(); //FIXME: this will fail if there's no stable data
-  t.equal(graph["West Bree"]["Esteldin"], 1);
-  t.end();
-});
-
 
 test("create graph with options, but level reqs not met", function(t) {
   var options = fatb.setup({level: 29});
   var graph = fatb.createGraph(true);
 
   t.equal(graph["West Bree"]["Esteldin"], 101);
-  t.end();
-});
-
-test("create graph with options, but level reqs met", function(t) {
-  var options = fatb.setup({level: 30});
-  var graph = fatb.createGraph(true);
-
-  t.equal(graph["West Bree"]["Esteldin"], 1);
-  t.end();
-});
-
-test("create graph with options, but standing reqs not met", function(t) {
-  var options = fatb.setup({standing: []});
-  var graph = fatb.createGraph(true);
-
-  // t.equal(graph["Aughaire"]["Gabilshathur"], 101);
-  t.equal(graph["Harwick"]["Hytbold"], 101);
   t.end();
 });
 
@@ -56,13 +20,3 @@ test("create graph with options, but standing reqs met", function(t) {
   t.equal(graph["Harwick"]["Hytbold"], 1);
   t.end();
 });
-// test("compare graphs", function(t) {
-//   var options = fatb.setup({level: 50, standing: ["R1","R2"]});
-//   var graphWeighted = fatb.createGraph(options);
-//   var graph = fatb.createGraph();
-// 
-//   t.deepEquals(graph, graphWeighted);
-//   t.end();
-// });
-
-
