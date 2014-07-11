e60950a247bce34deb9e12c45ae63327f6ca8f96
Mark Sta Ana <booyaabooyaabooyaa@gmail.com>
2014-07-11 09:11:28 +0100

    FindPath returns an empty array of no viable route is available because can't get tape to catch errors

--- tests/kanye.js
+++ tests/kanye.js
@@ -8,23 +8,17 @@ var fatb = require('../lib/fromatobree');
 var start = "Ost Galadh";
 var finish = "Mekhem-bizru";
 
-// test("find a route,but with level reqs, but not quest reqs met", function(t) {
-//   var options = fatb.setup({level: 50});
-//   fatb.graph = fatb.createGraph(true);
-// 
-//   t.deepEquals(fatb.FindPath(start,finish), ["Ost Galadh", "Mekhem-bizru"]);
-//   t.end();
-// });
 
+// doesn't work
 test("fail to find a route with level reqs, but not quest reqs met", function(t) {
-  var options = fatb.setup({level: 50});
+  var options = fatb.setup({level: 10});
   fatb.graph = fatb.createGraph(true);
-  t.throws(fatb.FindPath(start,finish), "Error: Could not find a path from Ost Galadh to Mekhem-bizru");
+  // t.throws(fatb.FindPath(start,finish), "Error: Could not find a path from Ost Galadh to Mekhem-bizru");
+  t.deepEquals(fatb.FindPath(start,finish), []);
   t.end();
 });
 
-
-test("find a route,but with both level and quest reqs met", function(t) {
+test("find a route with both level and quest reqs met", function(t) {
   var options = fatb.setup({level: 50, standing: ["Q3"]});
   fatb.graph = fatb.createGraph(true);
 
@@ -32,18 +26,8 @@ test("find a route,but with both level and quest reqs met", function(t) {
   t.end();
 });
 
-test("find a route hobbiton suri kyle", function(t) {
-  var graph = fatb.createGraph(true);
-
-  var start = "Hobbiton";
-  var finish = "Suri-kyla";
-
-  t.deepEquals(fatb.FindPath(start, finish), [start, "West Bree", "Ost Forod", "Kauppa-kohta", "Pynti-peldot", finish]);
-  t.end();
-});
-
-test("find a route hobbiton suri kyle", function(t) {
-  var options = fatb.setup({level: 50, standing: "R3"});
+test("find a route from Hobbiton to Suri-kyla (standing)", function(t) {
+  var options = fatb.setup({level: 40, standing: "R3"});
   fatb.graph = fatb.createGraph(true);
 
   var start = "Hobbiton";
@@ -52,6 +36,26 @@ test("find a route hobbiton suri kyle", function(t) {
   t.deepEquals(fatb.FindPath(start, finish), [start, 'West Bree', finish]);
   t.end();
 });
+
+test("find a longer route from Hobbiton to Suri-kyla (no standing)", function(t) {
+  fatb.setup({}); //FIXME: new instances of object, unless you want duff routes
+  fatb.graph = fatb.createGraph(true);
+
+  var start = "Hobbiton";
+  var finish = "Suri-kyla";
+
+  t.deepEquals(fatb.FindPath(start, finish), [start, "West Bree", "Ost Forod", "Kauppa-kohta", "Pynti-peldot", finish]);
+  t.end();
+});
+
+// test case for tape.throws not working
+// function thrower() { console.log('asdsad'); throw new Error("This is a test"); }
+// 
+// test("Error test throw", function(t) {
+//   t.throws(thrower(), "Error: This is a test");
+//   t.end();
+// });
+
 // test("fail to find aroute from ost guruth to the twenty first hall", function(t) {
 //   var options = fatb.setup({level: 30});
 //   fatb.graph = fatb.createGraph(true);
@@ -59,17 +63,9 @@ test("find a route hobbiton suri kyle", function(t) {
 //   var start = fatb.GetPlace("Ost Gur")[0];
 //   var finish = fatb.GetPlace("Twenty")[0];
 // 
-//   t.throws(fatb.FindPath(start, finish), "Error: No path found from Ost Guruth to Twenty-first Hall because you have not met the minimum requirements.");
+//   var path = fatb.FindPath(start, finish);
+//   t.throws(path , "Error: No path found from " + start + " to " + finish + " because you have not met the minimum requirements.");
+//   t.equals(typeof(path), []);
 //   t.end();
 // });
 
-
-// test("create graph with options, but standing reqs met", function(t) {
-//   var options = fatb.setup({standing: ["R28","Q6"]});
-//   var graph = fatb.createGraph(true);
-// 
-//   t.equal(graph["Harwick"]["Hytbold"], 1);
-//   t.end();
-// });
-//
-//
