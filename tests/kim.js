fdbf3936696448b4febfd05decef5080eed46354
Mark Sta Ana <booyaabooyaabooyaa@gmail.com>
2014-07-14 18:36:47 +0100

    fixed kim after switch to oopish

--- tests/kim.js
+++ tests/kim.js
@@ -2,35 +2,44 @@
 
 // testing non-path finding functionality
 var test = require('tape').test;
+var FATB = require('../lib/fromatobree');
+
 // currently broken...
 // var fatb = require('../lib/fromatobree');
 // fatb.setup();
 // fatb.createGraph();
 
-// test("find places that start with thor", function(t) {
-//   t.deepEquals(fatb.GetPlace("thor"), ["Thorenhad", "Thorin's Gate", "Thornhope"]);
-//   t.end();
-// });
-// 
-// test("find a place that starts with south", function(t) {
-//   t.deepEquals(fatb.GetPlace("south"), ["South Bree"]);
-//   t.end();
-// });
-// 
-// test("there should be no place caled xyz", function(t) {
-//   t.deepEquals(fatb.GetPlace("xyz"), []);
-//   t.end();
-// });
-// 
-// test("Get list of places in the shire", function(t) {
-//   var placesByRegion = fatb.GetPlacesByRegion("the Shire");
-//   t.deepEquals(placesByRegion, ["Brockenborings", "Hobbiton", "Michel Delving", "Needlehole", "Shire Homesteads", "Stock"]);
-//   t.end();
-// });
-// 
-// test("Get a list of regions", function(t) {
-//   var regions = fatb.GetRegions();
-// 
-//   t.deepEquals(regions, ["Angmar","Bree-land","Dunland","East Rohan","Enedwaith","Ered Luin","Eregion","Ettenmoors","Evendim","Forochel","Gap of Rohan","Great River","Lone-lands","Lothlorien","Mirkwood","Misty Mountains","Moria","Nan Curunir","North Downs","Trollshaws","West Rohan","Wildermore","the Shire"]);
-//   t.end();
-// });
+var fatb = new FATB({weighting: true, level: 65});
+test("find places that start with thor with weighting", function(t) {
+  t.deepEquals(fatb.GetPlace("thor"), ["Thorenhad", "Thorin's Gate", "Thornhope"]);
+  t.end();
+});
+
+fatb = new FATB();
+test("find places that start with thor w/o weighting (same applies for remaining", function(t) {
+  t.deepEquals(fatb.GetPlace("thor"), ["Thorenhad", "Thorin's Gate", "Thornhope"]);
+  t.end();
+});
+test("find a place that starts with south", function(t) {
+  t.deepEquals(fatb.GetPlace("south"), ["South Bree"]);
+  t.end();
+});
+
+test("there should be no place caled xyz", function(t) {
+  t.deepEquals(fatb.GetPlace("xyz"), []);
+  t.end();
+});
+
+test("Get list of places in the shire", function(t) {
+  var placesByRegion = fatb.GetPlacesByRegion("the Shire");
+  t.deepEquals(placesByRegion, ["Brockenborings", "Hobbiton", "Michel Delving", "Needlehole", "Shire Homesteads", "Stock"]);
+  t.end();
+});
+
+test("Get a list of regions", function(t) {
+  var regions = fatb.GetRegions();
+
+  t.deepEquals(regions, ["Angmar","Bree-land","Dunland","East Rohan","Enedwaith","Ered Luin","Eregion","Ettenmoors","Evendim","Forochel","Gap of Rohan","Great River","Lone-lands","Lothlorien","Mirkwood","Misty Mountains","Moria","Nan Curunir","North Downs","Trollshaws","West Rohan","Wildermore","the Shire"]);
+  t.end();
+});
+
